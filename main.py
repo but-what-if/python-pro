c451ce0be53a82df37c395570548d40225392e5d
but-what-if <bondar.vladimir.all@gmail.com>
2020-12-25 13:47:14 +0200

    added function requirements and generate_users

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-# Hello
+from flask import Flask
+from faker import Faker
+import string
+import random
+
+def generate_str():
+    str = ''
+    for _ in range(8):
+        str += random.choice(string.ascii_lowercase().split(''))
+    return str
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello'
+
+
+@app.route('/requirements/')
+def requirements():
+    with open('./requirements.txt', 'r') as file:
+        data = file.read()
+    return data
+
+
+@app.route('/generate-users/')
+def generate_users():
+    fake = Faker()
+    names = []
+    for _ in range(100):
+        names.append(fake.name())
+    emails = [f'{"".join(name.split(" "))}.{generate_str()}@mail.com' for name in names]
+    names_str = '----'.join(emails)
+    return names_str
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
